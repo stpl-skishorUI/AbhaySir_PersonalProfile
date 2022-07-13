1f7803d752240b49d080650499dc984bef941559
somnath khabale <somnathkhabale1998@gmail.com>
2022-07-13 14:28:04 +0530

    post api call

--- js/index.js
+++ js/index.js
@@ -1,27 +1,4 @@
-$('.owl-carousel').owlCarousel({
-    loop: true,
-    margin: 10,
-    nav: true,
-    startPosition: 0,
-    margin: 10,
-    merge: true,
-    video: true,
-    lazyLoad: true,
-    center: true,
-    // videoWidth: false,
-    // videoHeight: false,
-    responsive: {
-        0: {
-            items: 1
-        },
-        600: {
-            items: 3
-        },
-        1000: {
-            items: 5
-        }
-    }
-});
+
 
 // Example starter JavaScript for disabling form submissions if there are invalid fields
 (() => {
@@ -37,3 +14,48 @@ $('.owl-carousel').owlCarousel({
     }, false)
   })
 })()
+
+$(document).ready(function () {
+  // saveData();
+  saveData();
+});
+
+function saveData() {
+  let PersonName = $('#PersonName').val();
+  let MobileNo = $('#MobileNo').val();
+  let WardNo = $('#WardNo').val();
+  let LocationName = $('#LocationName').val();
+  let Description = $('#Description').val();
+
+  if (PersonName == "" || MobileNo == "" || WardNo == "") {
+
+  }
+  let queryparams;
+  queryparams = 'PersonName=' + PersonName + '&MobileNo=' + MobileNo+ '&WardNo=' + WardNo+ '&LocationName=' + 'ddsf' + '&Description=' + 'sdfds';
+  //console.log(JSON.stringify(queryparams));
+  $.ajax({
+      type: "POST",
+      url: "http://electionsurvey.erpguru.in/service.asmx/insert_surveydata_1_0?" + queryparams,
+      headers: { 'Access-Control-Allow-Headers': 'Origin, Content-Type, Accept', 'Accept': 'application/json,application/text,*/*' },
+      //data: JSON.stringify(queryparams),
+      dataType: "json",
+      contentType: "text/plain",
+      processdata: false,
+      success: function (data) {
+      },
+      error: function () {
+          ///alert("Error Occured");
+      }
+  })
+
+}
+
+function validateMobileNumber(mobileNo) {
+  var regex = /(^[6-9]\d{9}$)/;
+  var filter = /([0-9]{10})|(\([0-9]{3}\)\s+[0-9]{3}\-[0-9]{4})/;
+  if (regex.test(mobileNo)) {
+      return true;
+  } else {
+      return false;
+  }
+}
